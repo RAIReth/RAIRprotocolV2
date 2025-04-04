72d4aacb7e83a9a142e6b79efbecb17484c5cf00
Garrett Minks <garrettminks@gmail.com>
2025-04-04 15:12:08 -0600

    Fix navigation and footer data structure, add favicon.svg

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,37 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
-import type { HeaderData } from './types/navigation';
+import type { HeaderData, FooterData } from './types/navigation';
 
 export const headerData: HeaderData = {
   links: [],
   actions: [{ text: 'GitHub', href: 'https://github.com/rairprotocol', target: '_blank' }],
+};
+
+export const footerData: FooterData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Documentation', href: '#' },
+        { text: 'Pricing', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms of Service', href: '#' },
+    { text: 'Privacy Policy', href: '#' },
+  ],
+  socialLinks: [
+    { text: 'Twitter', href: 'https://twitter.com/rairprotocol', icon: 'tabler:brand-twitter' },
+    { text: 'GitHub', href: 'https://github.com/rairprotocol', icon: 'tabler:brand-github' },
+  ],
+  footNote: '© 2023 RAIRprotocol. All rights reserved.',
 }; 
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -13,4 +13,24 @@ export type Action = {
 export type HeaderData = {
   links: Link[];
   actions: Action[];
+};
+
+export type FooterLink = {
+  text: string;
+  href: string;
+  ariaLabel?: string;
+  icon?: string;
+};
+
+export type FooterLinks = {
+  title?: string;
+  links: Array<FooterLink>;
+};
+
+export type FooterData = {
+  links: Array<FooterLinks>;
+  secondaryLinks: Array<FooterLink>;
+  socialLinks: Array<FooterLink>;
+  footNote?: string;
+  theme?: string;
 }; 
